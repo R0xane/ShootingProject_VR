ae073a866f637126e05653be429a2a17378f509a
Roxane Braconnier <101722065+R0xane@users.noreply.github.com>
2025-11-23 18:06:06 +0100

    Sound effects

--- Assets/Scripts/GunShoot.cs
+++ Assets/Scripts/GunShoot.cs
@@ -4,10 +4,12 @@ public class GunShoot : MonoBehaviour
 {
     public ParticleSystem muzzleFlash;
     public Light muzzleLight;
+    public AudioSource gunAudio;
 
 
     public void Fire()
     {
+        gunAudio.Play();
         if (muzzleFlash != null)
         {
             muzzleFlash.Play(); 
